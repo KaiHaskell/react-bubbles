8acc0c27c831d8641242b80916ddc0e403984bc3
Kai <31229627+KaiHaskell@users.noreply.github.com>
2020-01-31 09:25:09 -0500

    Completed login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = props => {
+  const [user, setUser] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(user);
+
+    axios
+      .post("http://localhost:5000/api/login", user)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubbles");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div className="container-fluid">
+        <div className="row no-gutter">
+          <div className="col-md-8 col-lg-6">
+            <div className="login d-flex align-items-center py-5">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-9 col-lg-8 mx-auto">
+                    <h3 className="login-heading mb-4">Welcome back!</h3>
+                    <form onSubmit={handleSubmit}>
+                      <div className="form-label-group">
+                        <input
+                          type="text"
+                          name="username"
+                          value={user.username}
+                          placeholder="Enter Username"
+                          onChange={handleChange}
+                          className="form-control"
+                        />
+                        <label htmlFor="inputEmail">Username</label>
+                      </div>
+
+                      <div className="form-label-group">
+                        <input
+                          type="password"
+                          name="password"
+                          value={user.password}
+                          placeholder="Enter Password"
+                          onChange={handleChange}
+                          className="form-control"
+                        />
+                        <label htmlFor="inputPassword">Password</label>
+                      </div>
+
+                      <div className="custom-control custom-checkbox mb-3">
+                        <input
+                          type="checkbox"
+                          className="custom-control-input"
+                          id="customCheck1"
+                        />
+                      </div>
+                      <button
+                        className="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2"
+                        type="submit"
+                      >
+                        Sign in
+                      </button>
+                      <div className="text-center">
+                        <a className="small" href="#">
+                          Forgot password?
+                        </a>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
